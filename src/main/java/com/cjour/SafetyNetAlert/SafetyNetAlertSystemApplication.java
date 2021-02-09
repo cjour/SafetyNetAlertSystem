ae468b811f6e028b66e31a3a5f0b794ed8eb727c
Clément <jour.clement@gmail.com>
2021-02-09 14:20:52 +0100

    databse injection

--- src/main/java/com/cjour/SafetyNetAlert/SafetyNetAlertSystemApplication.java
+++ src/main/java/com/cjour/SafetyNetAlert/SafetyNetAlertSystemApplication.java
@@ -1,5 +1,6 @@
 package com.cjour.SafetyNetAlert;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +10,9 @@ import com.cjour.SafetyNetAlert.repository.Database;
 @SpringBootApplication
 public class SafetyNetAlertSystemApplication {
 
+@Autowired
+Database database;
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertSystemApplication.class, args);
-		Database db = new Database();
 	}
 }
